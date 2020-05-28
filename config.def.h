c0da9328f95b9cce3ba761200f703e9a5d71dbec
Faraaz Sareshwala <faraazs@gmail.com>
2020-05-28 14:09:12 -0700

    config: set symbol for tile

--- config.def.h
+++ config.def.h
@@ -39,7 +39,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[T]",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 };
 
