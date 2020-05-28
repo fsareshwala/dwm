106539197a9e76933ac68c14eabc9953e80d6bb7
Faraaz Sareshwala <faraazs@gmail.com>
2020-05-28 14:09:12 -0700

    config: set symbol for tile

--- config.def.h
+++ config.def.h
@@ -40,7 +40,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+	{ "[T]",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 };
 
