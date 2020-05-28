f0a12bb0add04ca2152af31abaa77724a4c9e0a3
Faraaz Sareshwala <faraazs@gmail.com>
2020-05-28 13:47:17 -0700

    config: add keybindings for programs

--- config.def.h
+++ config.def.h
@@ -59,10 +59,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
+static const char *abbrcmd[] = { "/home/fsareshwala/prefix/bin/abbr", NULL };
+static const char *flameshotcmd[] = { "flameshot", "full", "-p", "/home/fsareshwala", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = abbrcmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = flameshotcmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
