5deae5eddeeadd4c068227bb714256f638a56135
Faraaz Sareshwala <faraazs@gmail.com>
2020-08-20 16:48:21 -0700

    config: update colors

--- config.def.h
+++ config.def.h
@@ -15,11 +15,14 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_blue[]        = "#3182bd";
+static const char col_green[]       = "#31a354";
+static const char col_orange[]      = "#e6550d";
+static const char col_red[]         = "#e31a1c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_blue,  col_green  },
 };
 
 /* tagging */
@@ -62,7 +65,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "/home/fsareshwala/code/dmenu/dmenu_run", "-c", "-l", "50", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "/home/fsareshwala/code/dmenu/dmenu_run", "-c", "-l", "50", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 
 static const char scratchpadname[] = "scratchpad";
