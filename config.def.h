56787ad9aa68f71253d9bbe5f290e804cd2cc5e9
Faraaz Sareshwala <faraazs@gmail.com>
2022-03-31 02:18:28 -0700

    config: move font to a define to make it constant

--- config.def.h
+++ config.def.h
@@ -3,6 +3,8 @@
 
 #include <X11/XF86keysym.h>
 
+#define font "terminus:size=10"
+
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 15;       /* gaps between windows */
@@ -10,9 +12,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *font             = "terminus:size=10";
 static const char *fonts[]          = { font };
-static const char *dmenufont        = font ;
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
