bdcbc5eb0dc54c0c4ba026d5736a6364b146a71f
Faraaz Sareshwala <faraazs@gmail.com>
2022-08-02 00:33:48 -0700

    config: format

--- config.def.h
+++ config.def.h
@@ -107,8 +107,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = gappx  } },
 	{ 0,                            XF86XK_AudioMute, spawn,   SHCMD("amixer sset Master toggle") },
-	{ 0,                            XF86XK_AudioLowerVolume, spawn, SHCMD("amixer sset Master 5%-") },
-	{ 0,                            XF86XK_AudioRaiseVolume, spawn, SHCMD("amixer sset Master 5%+") },
+	{ 0,                            XF86XK_AudioLowerVolume,   spawn, SHCMD("amixer sset Master 5%-") },
+	{ 0,                            XF86XK_AudioRaiseVolume,   spawn, SHCMD("amixer sset Master 5%+") },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
